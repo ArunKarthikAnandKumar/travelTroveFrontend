55a7a8738d59bb247117bc4e13b3954d49ab63b8
Arun Karthik AnandKumar <137405963+ArunKarthikAnandKumar@users.noreply.github.com>
2025-10-31 16:01:17 +0530

    Update AdminRoutes.tsx

---
+++ src/routes/AdminRoutes.tsx
@@ -0,0 +1,35 @@
+import { Routes, Route } from "react-router-dom";
+import AdminLayout from "../layout/AdminLayout";
+import NotFound from "../pages/Visitor/NotFound";
+import AdminHome from "../pages/Admin/AdminHome";
+import ContinentPage from "../pages/Admin/Continent/ContinentPage";
+import CountryPage from "../pages/Admin/Country/CountryPage";
+import StatePage from "../pages/Admin/State/StatePage";
+import CityPage from "../pages/Admin/City/CityPage";
+import AttractionPage from "../pages/Admin/Attraction/AttractionPage";
+import RestaurantPage from "../pages/Admin/Restaurant/RestaurantPage";
+import HotelPage from "../pages/Admin/Hotel/HotelPage";
+import DestinationGuidePage from "../pages/Admin/DestinationGuide/DestinationGuidePage";
+import ItineraryPage from "../pages/Admin/Itinerary/ItineraryPage";
+
+const AdminRoutes: React.FC = () => {
+  return (
+    <Routes>
+      <Route element={<AdminLayout />}>
+        <Route path="/home" element={<AdminHome />} />
+        <Route path="/continents" element={<ContinentPage />} />
+        <Route path="/countries" element={<CountryPage />} />
+        <Route path="/states" element={<StatePage />} />
+        <Route path="/cities" element={<CityPage />} />
+        <Route path="/attractions" element={<AttractionPage />} />
+        <Route path="/restaurants" element={<RestaurantPage />} />
+        <Route path="/hotels" element={<HotelPage />} />
+        <Route path="/itinerary" element={<ItineraryPage />} />
+        <Route path="/destinationGuides" element={<DestinationGuidePage />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AdminRoutes;
