c052702500f85ed2acef9fa973db3eeb884efca4
Arun Karthik AnandKumar <137405963+ArunKarthikAnandKumar@users.noreply.github.com>
2025-10-31 16:04:04 +0530

    Update constatnts.ts

---
+++ src/utils/constatnts.ts
@@ -0,0 +1,29 @@
+export const BASE_URL = "http://localhost:3000";
+
+export const COUNTRY_DATA = [
+  "France",
+  "Italy",
+  "USA",
+  "Japan",
+  "UK",
+  "Spain",
+  "Thailand",
+  "UAE",
+  "Australia",
+  "Turkey",
+  "Morocco",
+  "Brazil",
+  "South Africa",
+  "Singapore",
+  "Czech Republic",
+  "Netherlands",
+  "India",
+  "China",
+  "Greece",
+  "Hungary",
+  "Russia",
+  "Portugal",
+  "South Korea",
+  "Indonesia",
+  "Austria"
+];
