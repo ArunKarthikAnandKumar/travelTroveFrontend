27b63e7a2d130e40444a89fb28c57186a29daeaf
Arun Karthik AnandKumar <137405963+ArunKarthikAnandKumar@users.noreply.github.com>
2025-10-31 12:08:43 +0530

    Update AdminLayout.tsx

---
+++ src/layout/AdminLayout.tsx
@@ -0,0 +1,25 @@
+import { Navigate, Outlet } from "react-router-dom"
+import AdminNavbar from "../Components/NavBar/AdminNavbar"
+
+
+const AdminLayout:React.FC=()=>{
+    
+    console.log("Inside AdminLayout")
+    const isAuthenticated=sessionStorage.getItem("token")
+    const role=sessionStorage.getItem("role")
+
+    if(role!="admin"){
+        return <Navigate to="/admin/login" replace/>
+    }
+
+    return (
+        <>
+        <div className="d-flex flex-column min-vh-100">
+            <AdminNavbar/>
+
+        </div>
+
+        </>
+    )
+}
+export default AdminLayout
