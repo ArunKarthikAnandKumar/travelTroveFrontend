7f703dd25b9171567253bd1aa03580b43aedceb6
Arun Karthik AnandKumar <137405963+ArunKarthikAnandKumar@users.noreply.github.com>
2025-10-31 16:06:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,20 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import Home from './Components/Home'
-import Navbar from './Components/Navbar'
-import Concerts from './Components/Concerts'
-import BookConcert from './Components/BookConcert'
-import Login from './Components/Auth/Login'
-import Register from './Components/Auth/Register'
-import Purchases from './Components/Purchases'
-import UserProfile from './Components/Profile'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./css/Common.css";
+import "./css/Registration.css";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-
   return (
-    <>
-    <BrowserRouter>
-    <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/Home' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path='/purchases' element={<Purchases/>} />
-        <Route path='/profile' element={<UserProfile/>} />
-        <Route path='/view-concerts' element={<Concerts/>} />
-        <Route path='/book-concerts/:id' element={<BookConcert/>} />
-
-        
-
-      </Routes>
-
-    </BrowserRouter>
-   
-    </>
-  )
+    <div className="App">
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
