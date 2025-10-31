598923f012732e8001587c28dd817bcedf8dbd35
Arun Karthik AnandKumar <137405963+ArunKarthikAnandKumar@users.noreply.github.com>
2025-10-31 12:09:23 +0530

    Update UserLayout.tsx

---
+++ src/layout/UserLayout.tsx
@@ -0,0 +1,25 @@
+import { Navigate, Outlet } from "react-router-dom"
+import UserNavbar  from "../Components/NavBar/UserNavBar"
+
+const UserLayout:React.FC=()=>{
+    
+    const isAuthenticated=sessionStorage.getItem("token")
+    console.log('The token is',isAuthenticated)
+
+    if(!isAuthenticated){
+        return <Navigate to="/login" replace/>
+    }
+
+
+    return (
+        <>
+        <div className="d-flex flex-column min-vh-100">
+            <UserNavbar/>
+            <Outlet/>
+
+        </div>
+
+        </>
+    )
+}
+export default UserLayout
