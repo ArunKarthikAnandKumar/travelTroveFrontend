c87557f3e327920ca1bf60d60a0a5bbba0126bc2
Arun Karthik AnandKumar <137405963+ArunKarthikAnandKumar@users.noreply.github.com>
2025-10-31 11:37:10 +0530

    Update UserNavbar.tsx

---
+++ src/components/NavBar/UserNavbar.tsx
@@ -0,0 +1,40 @@
+import { Link, useNavigate } from "react-router-dom"
+import { clearSession } from "../../utils/token"
+
+const UserNavbar:React.FC=()=>{
+    const navigate=useNavigate()
+    const logout=()=>{
+      clearSession()
+      setTimeout(()=>{
+        navigate('/')
+      },1000)
+    }
+
+  
+    return (
+        <>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a className="navbar-brand" href="#">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Features</a>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/user/login" onClick={()=>logout()}>Logout</Link>
+      </li>
+      
+    </ul>
+  </div>
+</nav>
+
+        </>
+    )
+}
+export default UserNavbar
